d1091fc8c5b808a9a78f13ca33a6b381499a7f6a
nileshhazra <nileshhz99@gmail.com>
2023-08-13 19:33:01 +0530

    add validations

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,40 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
 
-        System.out.println("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+        System.out.println("Principal ($1k - $1m): ");
+        principal = scanner.nextInt();
+        if(principal >= 1000 && principal <= 1_000_000) {
+            break;
+        }
+        System.out.println("Enter a value between 1000 and 1000000");
+        }
 
+
+        while(true) {
         System.out.println("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT /MONTHS_IN_YEAR;
+        if(annualInterest >=1 && annualInterest <=30) {
+            monthlyInterest = annualInterest / PERCENT /MONTHS_IN_YEAR;
+            break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
+        while(true) {
         System.out.println("Period (Years): ");
         byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        if(years >= 1 && years <= 30) {
+            numberOfPayments = years * MONTHS_IN_YEAR;
+            break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+
+        }
 
         double mortgage = principal
                 * (monthlyInterest *Math.pow(1 + monthlyInterest, numberOfPayments ))
